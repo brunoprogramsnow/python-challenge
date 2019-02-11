7c8d4194b4263f09b49709a2639be605e815fed8
Bruno Marques <bmarques8989@gmail.com>
2019-02-10 21:28:27 -0500

    finished

--- PyBank/main.py
+++ PyBank/main.py
@@ -15,40 +15,37 @@ with open(file, newline="") as csvfile:
     print("Financial Analysis")
     print("---------------------------------------------")
     
-    
     month_count = []
     sum_rev = 0
-    #The total number of months included in the dataset
+    rev_vals = []
+    
     for row in fileReader:
         
         month_count.append(row[0])  
         sum_rev += int(row[1])
+        rev_vals.append(row[1])
     
+    avg_start = int(rev_vals[0])
+    avg_end = int(rev_vals[len(rev_vals) - 1])
+    change_in_avg = (avg_end - avg_start)/(len(month_count) - 1)
+
     print(f'Total Months: {len(month_count)}')
     print(f'Total Rev: {sum_rev}')
-   
-   
+    print(f'Average Change:{change_in_avg}')
+
+    deInProfts = []
+    counter = 0
+    for i in rev_vals:
+        if counter >= 1:
+            deInProfts.append(int(rev_vals[counter]) - int(rev_vals[counter -1]))
+            counter += 1
+        else:
+            counter += 1
     
+    increase_date = month_count[deInProfts.index(max(deInProfts)) + 1]
+    decrease_date = month_count[deInProfts.index(min(deInProfts)) + 1]
     
-    # maxList = []
-    # for row in fileReader:
-    #     maxList.append(row[1])
-        
-    # print(max(maxList))
+    print(f'Greatest Increase in Profits: {increase_date} {max(deInProfts)}')
+    print(f'Greatest Decrease in Profits: {decrease_date} {min(deInProfts)}')
 
-
-    
-    
-
-
-
-
-#The total number of months included in the dataset
-
-#The net total amount of "Profit/Losses" over the entire period
-
-#The average of the changes in "Profit/Losses" over the entire period
-
-#The greatest increase in profits (date and amount) over the entire period
-
-#The greatest decrease in losses (date and amount) over the entire period
+    print("---------------------------------------------")
